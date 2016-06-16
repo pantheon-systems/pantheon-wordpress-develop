e83bd733b663508955198b3595380f83d5a80633
Daniel Bachhuber <daniel@handbuilt.co>
2016-06-16 06:28:58 -0700

    Test runner should exit with PHPUnit's return

--- templates/test-runner.php
+++ templates/test-runner.php
@@ -8,5 +8,5 @@
 WP_CLI::add_command( 'phpunit', function(){
 	$path = dirname( __FILE__ );
 	passthru( $path . '/vendor/bin/phpunit', $exit_code );
-	WP_CLI::success( "{$exit_code}" );
+	exit( $exit_code );
 });
