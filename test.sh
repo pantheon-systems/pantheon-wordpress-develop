94ee6fb528e6ee7605cefccbdae5b87cf1573f81
Daniel Bachhuber <daniel@handbuilt.co>
2016-06-16 06:22:44 -0700

    Run the PHPUnit test suite

--- test.sh
+++ test.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 set -ex
+
+###
+# Get all necessary environment details.
+###
+PANTHEON_SSH_USER=$(terminus site connection-info --field=sftp_username --site=$PANTHEON_SITE --env=$PANTHEON_BRANCH)
+PANTHEON_SSH_HOST=$(terminus site connection-info --field=sftp_host --site=$PANTHEON_SITE --env=$PANTHEON_BRANCH)
+
+###
+# Run the tests
+###
+ssh -p 2222 -o StrictHostKeyChecking=no $PANTHEON_SSH_USER@$PANTHEON_SSH_HOST wp phpunit
