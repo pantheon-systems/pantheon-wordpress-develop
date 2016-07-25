a264e048de295bfe8927fa5751e90d5b89b94da6
Daniel Bachhuber <daniel@handbuilt.co>
2016-07-25 16:23:47 -0700

    Run the entire test suite

--- templates/test-runner.php
+++ templates/test-runner.php
@@ -9,6 +9,6 @@ WP_CLI::add_command( 'phpunit', function(){
 	$path = dirname( __FILE__ );
 	$contents = file_get_contents( $path . '/latest-changeset.txt' );
 	echo PHP_EOL . '## LATEST CHANGESET' . PHP_EOL . PHP_EOL . $contents . PHP_EOL;
-	passthru( $path . '/vendor/bin/phpunit --filter Tests_Category', $exit_code );
+	passthru( $path . '/vendor/bin/phpunit', $exit_code );
 	exit( $exit_code );
 });
