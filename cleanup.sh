f99dc80f66f36038166c70c070b61796f2873872
Daniel Bachhuber <daniel@handbuilt.co>
2016-07-26 12:35:33 -0700

    Force the `$SLACK_CHANNEL` to be escaped

--- cleanup.sh
+++ cleanup.sh
@@ -14,6 +14,7 @@ set +x
 if [ ! -z "$SLACK_WEBHOOK" ] && [ ! -z "$SLACK_CHANNEL" ]; then
 	PANTHEON_SSH_USER=$(terminus site connection-info --field=sftp_username)
 	PANTHEON_SSH_HOST=$(terminus site connection-info --field=sftp_host)
+	SLACK_CHANNEL=${SLACK_CHANNEL/#/\\#}
 	ssh -p 2222 -o StrictHostKeyChecking=no $PANTHEON_SSH_USER@$PANTHEON_SSH_HOST wp phpunit-report $SLACK_WEBHOOK $SLACK_CHANNEL $CIRCLE_BUILD_URL
 fi
 set -x
